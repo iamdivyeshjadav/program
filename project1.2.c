2f55514f672c8af85a70779b7cd463b9537f063b
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-04-02 18:30:49 +0530

    Add files via upload

---
+++ project1.2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	float a,b,c,d,e;
+	
+	printf("base salary\n");
+	scanf("%f",&a);
+	printf("HRA salary\n");
+	scanf("%f",&b);
+	printf("DA salary\n");
+	scanf("%f",&c);
+	printf("TA salary\n");
+	scanf("%f",&d);
+	e=a+(b/100.0*a)+(c/100.0*a)+(d/100.0*a);
+	printf("Gross salary %f",e);
+}
