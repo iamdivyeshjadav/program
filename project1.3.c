9d615d3afa706e12f57fcd955e404895a0f6f77f
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-04-03 16:53:50 +0530

    Add files via upload

---
+++ project1.3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("first angle\n");
+	scanf("%d",&a);
+	printf("second angle\n");
+	scanf("%d",&b);
+	c=180-(a+b);
+	printf("ans %d:",c);
+
+}
+/*
+*/
