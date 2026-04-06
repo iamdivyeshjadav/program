5bd8edf9565533ebe90fc223370d9280dc52ff2d
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-04-06 18:04:45 +0530

    Add files via upload

---
+++ PR 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int marks;
+	printf("enter marks\n");
+    scanf("%d",&marks);
+    if(marks>85);
+    printf("Grede A");
+    if(85>70);
+    printf("Grede B");
+ 	
+}
