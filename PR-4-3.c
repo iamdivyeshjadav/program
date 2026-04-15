dcd28ad6010a467515e593d120ef0af9b7a24ef0
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-04-15 16:56:05 +0530

    Add files via upload

---
+++ PR-4-3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	for(i=5;i>=1;i--){
+		for(n=1;n<i;n++)
+		printf("  ");
+		for(j=i;j<=5;j++)
+		printf("%d ",j);
+		printf("\n");
+	}
+}
+/*
+output
+        5
+      4 5
+    3 4 5
+  2 3 4 5
+1 2 3 4 5
+*/
