2a912c71e05d7728695c3a5978070788a55a931c
Divyesh Jadav <divyeshjadav2008@gmail.com>
2026-04-22 16:25:49 +0530

    Add files via upload

---
+++ PR-6-2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+	char ch[50];
+	int d,count;
+	printf("Enter a string : ");
+	scanf("%s",ch);
+	printf("\n Frequency of each letter :");
+	for(d=0;ch[d]!='\0';d++){
+		count =1;
+		if(ch[d]=='0')
+		continue;
+		int k;
+		for(k=d+1;ch[k]!='\0';k++){
+			if(ch[d]==ch[k]){
+			count++;
+			ch[k]='0';
+			}
+		}
+		printf("\n%c => %d",ch[d],count);
+	}
+}
+/*
+output
+Enter a string: discipline
+
+ Frequency of each letter :
+d => 1
+i => 3
+s => 1
+c => 1
+p => 1
+l => 1
+n => 1
+e => 1
+*/
